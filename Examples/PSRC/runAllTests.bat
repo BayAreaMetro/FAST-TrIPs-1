2f175429279e7e627db49e4aaf79108a3259f31b
Lisa Zorn <lzorn@mtc.ca.gov>
2015-05-29 12:50:25 -0700

    Move runTest.py up a dir so we can use it for other examples

--- Examples/PSRC/runAllTests.bat
+++ Examples/PSRC/runAllTests.bat
@@ -5,20 +5,20 @@
 ::
 
 :pax200_deterministic_iter1_nocap
-python runTest.py 200 deterministic 1 0
+python ..\runTest.py 200 deterministic 1 0
 
 :pax200_deterministic_iter1_cap1
-python runTest.py 200 deterministic 1 1
+python ..\runTest.py 200 deterministic 1 1
 
 :pax200_deterministic_iter2_cap1
-python runTest.py 200 deterministic 2 1
+python ..\runTest.py 200 deterministic 2 1
 
 :: stochastic
 :pax200_stochastic_iter1_nocap
-python runTest.py 200 stochastic 1 0
+python ..\runTest.py 200 stochastic 1 0
 
 :pax200_stochastic_iter1_cap1
-python runTest.py 200 stochastic 1 1
+python ..\runTest.py 200 stochastic 1 1
 
 :pax200_stochastic_iter2_cap1
-python runTest.py 200 stochastic 2 1
+python ..\runTest.py 200 stochastic 2 1
